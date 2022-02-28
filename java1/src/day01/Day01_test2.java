de7a0e16b9a7d68c5d72069623c1dff53cbd0ee6
godoklife <96913067+godoklife@users.noreply.github.com>
2022-02-28 21:28:08 +0900

    printf 표현 추가

--- java1/src/day01/Day01_test2.java
+++ java1/src/day01/Day01_test2.java
@@ -6,6 +6,8 @@ public class Day01_test2 {	// c s
 
 	public static void main(String[] args) {	// m s
 		
+		
+		/*
 		Scanner scan = new Scanner(System.in);
 		
 		System.out.println("방명록 입력을 받습니다");
@@ -19,11 +21,23 @@ public class Day01_test2 {	// c s
 		String date = scan.next();
 		
 		scan.close();
+		*/
+		String name = "강호동";
+		String data = "안녕하세요";
+		String date = "09:21";
 		
 		System.out.println("* -----------방명록-----------");
-		System.out.println("* |순번|작성자| 내용\t\t|날짜\t|");
-		System.out.println("* | 1 | " + name + "|" + data + "\t|"+ date + "|");
+		System.out.printf("%6s%-5s%-10s%-8s\n", "* |순번|","작성자|","내용", "|날짜|");
+		System.out.printf("%-6s|%-3s|%-11s|%5s|\n", "* |1", name, data, date);
 		System.out.println("* --------------------------");
+		
+		
+		System.out.printf("\n\n");
+		System.out.println("* -----------방명록-----------");
+		System.out.println("* |순번|작성자| 내용\t\t|날짜   |");
+		System.out.println("* | 1 | " + name + "|" + data + "\t|"+ date + " |");
+		System.out.println("* --------------------------");
+		
 		
 	}	// m e
 	
