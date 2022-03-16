d8b5fb574369c70ae93832b7bc19b731b73e8eba
504 <504@DESKTOP-03H65CS>
2022-03-16 14:03:28 +0900

    5교시 시작

---
+++ java1/src/Day11_220316_BoardApp/Controller.java
@@ -0,0 +1,31 @@
+package Day11_220316_BoardApp;
+
+import java.util.ArrayList;
+
+public class Controller {
+	// 모든 메서드는 static으로 선언.
+	
+	// 0. 게시물 리스트 [ 모든 게시물을 담는 리스트 ] 
+	public static ArrayList<Board> boardlist = new ArrayList<>();
+	// 1. 목록 메서드
+	public static void boardlist() {}
+	// 2. 쓰기 메서드
+	public static void write() {}
+	// 3. 보기 메서드
+	public static void view() {}
+	// 4. 수정 메서드
+	public static void update() {}
+	// 5. 삭제 메서드
+	public static void delete() {}
+	// 6. 덧글쓰기 메서드
+	public static void replywrite() {}
+	// 7. 게시물저장 메서드
+	public static void save() {}
+	// 8. 게시물불러오기 메서드
+	public static void load() {}
+	// 9. 덧글저장 메서드
+	public static void replysave() {}
+	// 10. 덧글불러오기 메서드
+	public static void replyload() {}
+	
+}
---
+++ java1/src/Day11_220316_BoardApp/Day11_04_BoardApp.java
@@ -0,0 +1,31 @@
+package Day11_220316_BoardApp;
+
+import java.util.Scanner;
+
+public class Day11_04_BoardApp {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		while(true) {
+			try {	// 예상되는 발생 예외 : 사용자가 숫자 이외의 값 입력.
+			Controller.boardlist();	// static으로 선언했으므로 객체없이 호출 가능!!!!
+			System.out.print("1.쓰기 2.보기 선택 >");
+			int ch = scanner.nextInt();
+			if(ch==1) {
+				
+			}
+			else if(ch==2) {
+				
+			}
+			else {
+				
+			}
+			} catch (Exception e) {
+				System.out.println("경고) 선택 가능한것만 누르시오.");
+				scanner = new Scanner(System.in);	
+					// 기존 스캐너를 다시 메모리 할당 해줌으로써 메모리를 비워줌.
+			}
+			
+		}	// while END
+	}	// main END
+}	// class END
