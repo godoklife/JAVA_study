776740de2b4ed15067cde6cd062dfe8a9826e5e7
504 <504@DESKTOP-Q5RV3BV>
2022-04-01 12:25:52 +0900

    1

--- KYJs_Project/src/application/Day22_01/Day22_01_controller.java
+++ KYJs_Project/src/application/Day22_01/Day22_01_controller.java
@@ -1,6 +1,8 @@
 package application.Day22_01;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
@@ -33,15 +35,49 @@ public class Day22_01_controller implements Initializable{
     private TextArea txtcontentlist;
 
     @FXML
+    void write(ActionEvent event) {
+    	System.out.println("DB내 데이터를 저장합니다.");
+    	
+    	String 작성자 = txtwriter.getText();
+    	String 내용 = txtcontent.getText();
+    	
+    	
+    	// 1. 
+    	Day22_02 DB연동 = new Day22_02();
+    	// 2. 
+    	boolean result = DB연동.write(작성자, 내용);
+    	
+    	if(result) {
+    		System.out.println("DB에 저장 성공");
+    		
+    		// 성공시 컨트롤에 입력된 데이터 지워주기.
+    		txtwriter.setText("");
+    		txtcontent.setText("");
+    	}else {
+    		System.out.println("DB에 저장을 실패했습니다.");
+    	}
+    }
+    
+    @FXML
     void get(ActionEvent event) {
     	System.out.println("DB내 데이터 호출");
     	
+    	// 1. DB 연동 객체 만들기
+    	Day22_02 DB연동 = new Day22_02();
+    	
+    	// 2. DB 객체 내 메서드 호출 후 결과물 리스트로 
+    	ArrayList<데이터> 데이터목록 = DB연동.get();
+    	
+    	for(데이터 tmp : 데이터목록) {
+    		txtcontentlist.appendText(
+    				"번호"+tmp.get번호()+
+    				"작성자"+tmp.get작성자()+
+    				"내용"+tmp.get내용());
+    	}
+    	
+    	
+    	
     }
-
-    @FXML
-    void write(ActionEvent event) {
-    	System.out.println("DB내 데이터를 저장합니다.");
-
-    }
+    
 	
 }
--- KYJs_Project/src/application/Day22_01/Day22_02.java
+++ KYJs_Project/src/application/Day22_01/Day22_02.java
@@ -2,6 +2,11 @@ package application.Day22_01;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import com.mysql.cj.xdevapi.PreparableStatement;
 
 public class Day22_02 {
 	
@@ -27,5 +32,79 @@ public class Day22_02 {
 		} catch (Exception e) {System.out.println("[ DB 연동 실패 ]"+ e);}
 	}
 	// 3. 메서드
+	
+	// 2) 삽입 메서드
+	public boolean write (String 작성자, String 내용) {
+		try {
+		// 1. SQL 작성 [ DB에 쓰기 ] 
+			// DB 내 테이블에 데이터 저장 : insert into 테이블명(필드명1, 필드명2) 
+			// values(필드명1의값, 필드명2의값)
+		String sql = "insert into test(writer, content) values(?, ?)";
+		// 2. SQl 설정 [ 연결된 DB에 SQL 설정 ]
+			// PreparedStatement : 연결된 DB에서 테이블 조작 ( 삽입, 삭제 ,수정, 생성)할때 쓰는 인터페이스
+		PreparedStatement ps = connection.prepareStatement(sql);
+		ps.setString(1, 작성자);
+		ps.setString(2, 내용);
+		// 3. SQL 실행
+		ps.executeUpdate();
+		return true;
+		
+		
+		
+		
+		
+		}catch(Exception e) {System.out.println("[ SQL 연결 실패 ]"+e);}
+		// 성공시 true
+		
+		// 실패시 false 반환
+		return false;
+	}
+	
+	
+	// 3) 호출 메서드
+	public ArrayList<데이터> read (){
+		
+	// *
+	ArrayList<데이터> 데이터목록 = new ArrayList<>();
+	
+	String sql = "select * from test";
+			// se
+	try {
+		PreparedStatement ps = connection.prepareStatement(sql);
+		ResultSet rs = ps.executeQuery();
+		
+		while(rs.next()) {
+			데이터 tmp = new 데이터(
+			rs.getInt(1),
+			rs.getString(2),
+			rs.getString(3));
+			
+			// 2. 객체를 리스트에 담아주기
+			데이터목록.add(tmp);
+			
+			// 성공시 데이터목록 리스트 리턴			
+			return 데이터목록;
+			
+		}
+	} catch (Exception e) {System.out.println("불러오기 실패"+e);}
+	
+	
+	
+
+	
+	// 실패시
+	return null;
+	
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
