de3d42082911218b48f9c18721e24e381458c5d1
504 <504@DESKTOP-2M11V1C>
2022-04-05 17:05:17 +0900

    1705 폰트변경

--- KYJs_Project/src/app/Start.java
+++ KYJs_Project/src/app/Start.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Start extends Application{
@@ -27,6 +28,14 @@ public class Start extends Application{
 				// 슬래쉬를 쓰면 상대경로로 인식, 역슬래쉬를 쓰면 상대경로로 인식한다.
 			// 2. 스테이지 설정			
 			arg0.getIcons().add(image);	// 스테이지 로고 적용
+///////////////////////////////////////////////////////////////////////////////////////////////////////			
+		// * 외부 폰트 일괄 적용 & application.css 연동
+			// 1. 폰트 호출
+			Font.loadFont(getClass().getResourceAsStream("CookieRun Regular.ttf"), 14);
+			// 2. 외부 스타일시트를 씬 객체에 적용
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+///////////////////////////////////////////////////////////////////////////////////////////////////////			
+			
 		arg0.setResizable(false);	// 4. 스테이지(창) 크기 재조정 불가 
 		arg0.setTitle("이젠마켙");	// 2. 스테이지 창 이름 설정
 		arg0.show();
